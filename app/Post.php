a34cded72c51474375f3b7238dffff164a0f76e9
DevelopJKong <ybaram40@gmail.com>
2022-05-23 16:04:11 +0900

    39 - 게시글 (Post) 완료

--- app/Post.php
+++ app/Post.php
@@ -1,18 +1,52 @@
 <?php
+
 namespace App;
 
 use Eclair\Database\Adaptor;
 
 class Post
 {
+
+    public function create()
+    {
+        return Adaptor::exec(
+            'INSERT INTO posts(`user_id`, `title`, `content`) VALUES(?, ?, ?)',
+            [ $this->user_id, $this->title, $this->content ]
+        );
+    }
+
+
+    public function update()
+    {
+        return Adaptor::exec(
+            'UPDATE posts SET `title` = ?, `content` = ? WHERE `id` = ?',
+            [$this->title, $this->content, $this->id]
+        );
+    }
+    public function delete()
+    {
+        return Adaptor::exec(
+            'DELETE FROM posts WHERE `id` = ?',
+            [$this->id]
+        );
+    }
+
     public function user()
     {
-        return current(Adaptor::getAll('SELECT * FROM users WHERE `id` =?',[$this->user_id],\App\User::class));
+        return current(Adaptor::getAll('SELECT * FROM users WHERE `id` =?', [$this->user_id], \App\User::class));
+    }
+
+    public function isOwner()
+    {
+        if(array_key_exists('user',$_SESSION)) {
+            return $this->user_id === $_SESSION['user']->id;
+        }
+        return false;
     }
 
     public function getCreatedAt()
     {
-        return date('h:i A, M j',strtotime($this->created_at));
+        return date('h:i A, M j', strtotime($this->created_at));
     }
 
     public function getUsername()
@@ -22,6 +56,11 @@ class Post
 
     public function getSummary()
     {
-        return filter_var(mb_substr(strip_tags($this->content),0,200),FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        return filter_var(mb_substr(strip_tags($this->content), 0, 200), FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    }
+
+    public static function get($id)
+    {
+        return current(Adaptor::getAll('SELECT * FROM posts WHERE `id` = ?',[ $id ], \App\Post::class));
     }
 }
--- public/app.js
+++ public/app.js
@@ -1,66 +1,48 @@
+/* global BalloonEditor */
+
 /**
- *  logout
+ * Logout
  */
-
-const logout = document.querySelector('#logout');
-if(logout instanceof HTMLElement) {
-    logout.addEventListener('click',() => {
-        fetch('/auth/login',{method:'POST'}).then(() => window.location.reload());
-    });
+const $logout = document.getElementById('logout')
+if ($logout instanceof HTMLElement) {
+    $logout.addEventListener('click', () => {
+        fetch('/auth/logout', { method: 'post' }).then(() => window.location.reload())
+    })
 }
-
-
-
-
 
 /**
  * Editor
- * @type {HTMLElement}
  */
-
-const editor = document.getElementById("editor");
-if (editor instanceof HTMLElement) {
-    BalloonEditor.create(editor, {
+const $editor = document.getElementById('editor')
+if ($editor instanceof HTMLElement) {
+    BalloonEditor.create($editor, {
         ckfinder: {
-            uploadUrl: "/image"
+            uploadUrl: '/images'
         }
     }).then(editor => {
-        editor.editing.view.focus();
-        const form = document.querySelector("#main__form-post > form");
-        form.addEventListener("sumbit", (event) => {
-            const data = document.createTextNode(editor.getData());
-            document
-                .querySelector("#main__form-post textarea[name=content]")
-                .appendChild(data);
-
-        });
-    });
+        editor.editing.view.focus()
+        const $form = document.querySelector('#main__form-post > form')
+        $form.addEventListener('submit', e => {
+            const data = document.createTextNode(editor.getData())
+            document.querySelector('#main__form-post textarea[name=content]').appendChild(data)
+        })
+    })
 }
 
 /**
- *  readmore
+ * Readmore
  */
-
-const readmore = document.getElementById("readmore");
-if(readmore instanceof HTMLElement) {
-    let page = 0;
-    readmore.addEventListener("click",() => {
-        fetch(`/?page=${++page}`,{
-            method: "GET"
-        }).then(async (response) => {
-            const parser = new DOMParser();
-            const doc = parser.parseFromString(await response.text(),"text/html");
-            const list = doc.querySelectorAll(".uk-container > .uk-list > li");
-            if(list.length > 0) {
-                Array.from(list).forEach(item => {
-                    document.querySelector(".uk-container > .uk-list").appendChild(item);
-                });
-            }
-        });
-    });
+const $readmore = document.getElementById('readmore')
+if ($readmore instanceof HTMLElement) {
+    let page = 0
+    $readmore.addEventListener('click', () => fetch('/?page=' + ++page, { method: 'get' }).then(async response => {
+        const parser = new DOMParser()
+        const doc = parser.parseFromString(await response.text(), 'text/html')
+        const list = doc.querySelectorAll('.uk-container > .uk-list > li')
+        if (list.length > 0) {
+            Array.from(list).forEach(item => {
+                document.querySelector('.uk-container > .uk-list').appendChild(item)
+            })
+        }
+    }))
 }
-
-
-
-
-
